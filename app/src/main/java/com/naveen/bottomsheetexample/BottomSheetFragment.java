733f7e2e4f6250e897b4ad0b82447cd937fc44f7
[RandheerNST] <randheer.newsparktechnology@gmail.com>
2019-01-29 11:44:30 +0530

    initial commit

---
+++ app/src/main/java/com/naveen/bottomsheetexample/BottomSheetFragment.java
@@ -0,0 +1,22 @@
+package com.naveen.bottomsheetexample;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomSheetDialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class BottomSheetFragment extends BottomSheetDialogFragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+       return inflater.inflate(R.layout.fragment_bottom_sheet_dialog, container, false);
+    }
+}
